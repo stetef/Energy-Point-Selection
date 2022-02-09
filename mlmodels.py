c036349cb8865ce824c4756d9678f62b03c9f22e
Samantha Tetef <stetef@u.rochester.edu>
2022-02-08 17:40:48 -0800

    added comparison to random selection of energy points

---
+++ mlmodels.py
@@ -0,0 +1,248 @@
+"""GAN models."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+import tensorflow as tf
+import tensorflow.keras as keras
+import keras.backend as K
+
+from keras.constraints import Constraint
+from keras.layers import Dense, BatchNormalization, LeakyReLU
+from keras.models import Sequential
+from keras.optimizers import RMSprop
+
+from utils import generate_linear_combos, get_coeffs_from_sepctra
+from utils import format_axis
+
+
+class ClipConstraint(Constraint):
+    """Custom weight clipping object."""
+
+    def __init__(self, clip_value):
+        """init."""
+        self.clip_value = clip_value
+
+    def __call__(self, weights):
+        """call."""
+        return tf.clip_by_value(weights, -self.clip_value, self.clip_value)
+
+    def get_config(self):
+        """config."""
+        return {'clip_value': self.clip_value}
+
+
+class WGAN:
+    """Wasserstein GAN."""
+
+    def __init__(self, refs, latent_dim):
+        """Init function."""
+        self.Refs = refs
+        self.y_shape = self.Refs.shape[1]
+        self.latent_dim = latent_dim
+        self.discriminator = self._define_discriminator(n_inputs=self.y_shape)
+        self.generator = self._define_generator(n_outputs=self.y_shape)
+        self.gan = self._define_gan(self.generator, self.discriminator)
+
+    def _discriminator_loss(self, real_data, fake_data):
+        """Discriminator loss."""
+        real_loss = tf.reduce_mean(real_data)
+        fake_loss = tf.reduce_mean(fake_data)
+        return fake_loss - real_loss
+
+    def _wasserstein_loss(self, y_true, y_pred):
+        """GAN loss for Wasserstein distance."""
+        return K.mean(y_true * y_pred)
+
+    def _define_discriminator(self, n_inputs=2):
+        """Initialize discriminator."""
+        constraint = ClipConstraint(0.01)
+        model = Sequential(name='Discriminator')
+        model.add(Dense(32, kernel_initializer='he_uniform',
+                        input_dim=n_inputs, kernel_constraint=constraint))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU())
+        model.add(Dense(1, activation='linear'))
+        optimizer = keras.optimizers.Adam(learning_rate=0.0002,
+                                          beta_1=0.5, beta_2=0.9)
+        model.compile(loss=self._discriminator_loss, optimizer=optimizer)
+        return model
+
+    def _define_generator(self, n_outputs=2):
+        """Initialize generator."""
+        model = Sequential(name='Generator')
+        model.add(Dense(32, kernel_initializer='he_uniform',
+                        input_dim=self.latent_dim))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU())
+        model.add(Dense(32, kernel_initializer='he_uniform',
+                        input_dim=32))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU())
+        model.add(Dense(n_outputs, activation='linear'))
+        return model
+
+    def _generate_latent_points(self, n):
+        """Create noise for generator input."""
+        return np.random.normal(loc=0., scale=0.5, size=(n, self.latent_dim))
+
+    def generate_fake_samples(self, n, training=True):
+        """Pass through generator."""
+        noise = self._generate_latent_points(n)
+        spectra = self.generator.predict(noise)
+        # spectra = (spectra + 1.0) / 2.0
+        if training:
+            return spectra.astype(np.float64), np.ones((n, 1))
+        else:
+            return spectra.astype(np.float64), get_coeffs_from_sepctra(spectra,
+                                                                       self.Refs)
+
+    def _define_gan(self, generator, critic):
+        """Create WGAN."""
+        for layer in critic.layers:
+            if not isinstance(layer, BatchNormalization):
+                layer.trainable = False
+        model = Sequential(name='GAN')
+        model.add(generator)
+        model.add(critic)
+        optimizer = keras.optimizers.Adam(learning_rate=0.0002,
+                                          beta_1=0.5, beta_2=0.9)
+        model.compile(loss=self._wasserstein_loss, optimizer=optimizer)
+        return model
+
+    def plot_history(self, critic_losses, gan_losses):
+        """Plot showing training loss."""
+        fig, ax = plt.subplots(figsize=(6, 4))
+        plt.plot(critic_losses, label='critic loss')
+        # plt.plot(gan_losses, label='gan loss')
+        xticks = np.array(ax.get_xticks(), dtype=int) + 1
+        ax.set_xticklabels(xticks, fontsize=16)
+        ax.set_yticklabels(np.array(ax.get_yticks(), dtype=float), fontsize=16)
+        ax.set_xlabel('Epoch', fontsize=18)
+        ax.set_ylabel('Loss', fontsize=18)
+        ax.tick_params(direction='in', width=2, length=8, which='major')
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+        plt.legend(fontsize=18, loc='center left', bbox_to_anchor=(1.0, 0.5))
+        plt.show()
+
+    def plot_input_output(self, m, Energy, Refs):
+        """GAN visualization."""
+        latent_points = self._generate_latent_points(m)
+        spectra = self.generator.predict(latent_points)
+        spectra = (spectra + 1) / 2.0
+
+        fig, axes = plt.subplots(figsize=(5 * m, 5), ncols=m)
+        plt.subplots_adjust(wspace=0)
+        for i in range(m):
+            ax = axes[i]
+            n = len(latent_points[i])
+            ax.plot(Energy, spectra[i], '-', linewidth=4, c=plt.cm.tab20(0),
+                    label='output')
+            ax.plot(np.linspace(min(Energy), max(Energy), n),
+                    latent_points[i], '-', linewidth=4, c=plt.cm.tab20(2),
+                    label='input')
+            format_axis(ax, ticks=(10, 20), fontsize=20)
+            ax.legend(fontsize=20, loc=4)
+        plt.show()
+
+    def train_critic(self, half_batch, losses, **kwargs):
+        """Train the discriminator."""
+        X_real, y_real = generate_linear_combos(self.Refs, **kwargs)
+        self.discriminator.train_on_batch(X_real, y_real)
+        X_fake, y_fake = self.generate_fake_samples(half_batch,
+                                                    training=True)
+        self.discriminator.train_on_batch(X_fake, y_fake)
+        critic_loss = self._discriminator_loss(real_data=X_real,
+                                               fake_data=X_fake)
+        losses.append(critic_loss)
+        return losses
+
+    def train_gan(self, n_batch):
+        """GAN training step."""
+        X_gan = self._generate_latent_points(n_batch)
+        y_gan = -np.ones((n_batch, 1))
+        g_loss = self.gan.train_on_batch(X_gan, y_gan)
+        return g_loss
+
+    def train(self, n_epochs=128, n_batch=128, n_critic=25,
+              verbose=True, plot=True):
+        """Training function."""
+        half_batch = int(n_batch / 2)
+        kwargs = {'N': half_batch, 'scale': 0.0, 'dropout': 0.1,
+                  'training': True}
+        critic_losses, gan_losses = [], []
+        for i in range(n_epochs):
+            critic_loss = []
+            for j in range(n_critic):
+                critic_loss = self.train_critic(half_batch, critic_loss,
+                                                **kwargs)
+            critic_losses.append(np.average(critic_loss))
+            gan_loss = self.train_gan(n_batch)
+            gan_losses.append(-gan_loss)
+            if verbose:
+                print(f'Epoch: {i + 1}', end='\r')
+        if plot:
+            self.plot_history(critic_losses, gan_losses)
+
+
+class WGAN_GP(WGAN):
+    """WGAN-GP."""
+
+    def __init__(self, gp_weight, *args):
+        """
+        Init function.
+
+        Attributes:
+            gp_weight - extra parameter for the weight of the graident penalty
+
+        Inherit all other attributes from a normal WGAN.
+
+        """
+        self.gp_weight = gp_weight
+        super().__init__(*args)
+
+    def gradient_penalty(self, batch_size, real_data, fake_data):
+        """
+        Calculate the gradient penalty.
+
+        This loss is calculated on an interpolated image
+        and added to the discriminator loss.
+        """
+        # Start with the interpolated data
+        alpha = tf.random.normal([batch_size, self.y_shape], 0.0, 1.0)
+        diff = fake_data - real_data
+        interpolated = real_data + alpha * diff
+
+        with tf.GradientTape() as gp_tape:
+            gp_tape.watch(interpolated)
+            # 1. Get the discriminator output for this interpolated data.
+            pred = self.discriminator(interpolated, training=True)
+
+        # 2. Calculate the gradients w.r.t to this interpolated data.
+        grads = gp_tape.gradient(pred, [interpolated])[0]
+        # 3. Calculate the norm of the gradients.
+        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=1))
+        gp = tf.reduce_mean((norm - 1.0) ** 2)
+        return tf.cast(gp, tf.float64)
+
+    def train_critic(self, half_batch, losses, **kwargs):
+        """Train the discriminator."""
+        X_real, y_real = generate_linear_combos(self.Refs, **kwargs)
+        self.discriminator.train_on_batch(X_real, y_real)
+        X_fake, y_fake = self.generate_fake_samples(half_batch,
+                                                    training=True)
+        self.discriminator.train_on_batch(X_fake, y_fake)
+        temp_cost = self._discriminator_loss(real_data=X_real,
+                                             fake_data=X_fake)
+        gp = self.gradient_penalty(half_batch, X_real, X_fake)
+        print(gp, temp_cost)
+        critic_loss = temp_cost + gp * self.gp_weight
+        losses.append(critic_loss)
+        with tf.GradientTape() as tape:
+            d_gradient = tape.gradient(critic_loss,
+                                       self.discriminator.trainable_variables)
+            self.discriminator.optimizer.apply_gradients(
+                zip(d_gradient, self.discriminator.trainable_variables)
+            )
+        return losses
--- utils.py
+++ utils.py
@@ -2,21 +2,17 @@
 
 import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.linear_model import Lasso
-
-from scipy.interpolate import interp1d, UnivariateSpline
-from scipy.optimize import minimize
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 from sklearn.metrics import *
+
+import umap
 
 mpl.rcParams['font.family'] = ['sans-serif']
 mpl.rcParams['font.sans-serif'] = ['Arial']
@@ -26,7 +22,7 @@ def read_tddft_spectrum_file(path):
     return np.loadtxt(path).T
 
 def format_axis(ax, energyrange=None, ticks=(5, 10), fontsize=20):
-    """Format axes for spectral viewing."""
+    """Format axes for spectra plots."""
     ax.set_yticks([])
     ax.set_xlim(energyrange)
     ax.set_xlabel('Energy (eV)', fontsize=fontsize)
@@ -64,7 +60,7 @@ def get_coeffs(n, dropout):
     return coeffs
 
 def generate_linear_combos(Refs, scale=0, N=10, dropout=0.5, training=True):
-    """Create linear combo dataset."""
+    """Create linear combo dataset from Refs."""
     n = len(Refs)
     Data = []
     Coeffs = []
@@ -82,6 +78,49 @@ def generate_linear_combos(Refs, scale=0, N=10, dropout=0.5, training=True):
     else:
         return np.array(Data), np.array(Coeffs)
 
+def make_scree_plot(data, n=5, threshold=0.95, show_first_PC=True):
+    """
+    Make PCA scree plot.
+    
+    Attributes:
+        data - Data on which to perform PCA analysis.
+            type = ndarray
+    kwargs:
+        n - The number of PC's to display/ keep on the x-axis.
+            default = 5
+            type = int
+        threshold - The variance cutoff to display.
+            default = 0.95
+            type = float
+        show_first_PC - Opt to display the first PC.
+            default = True
+            type = boolean
+    """
+    fig, ax = plt.subplots(figsize=(8,6))
+    pca = PCA()
+    pca_components = pca.fit_transform(data)
+
+    x = np.arange(n) + 1
+    cdf = [np.sum(pca.explained_variance_ratio_[:i + 1]) for i in range(n)]
+
+    ax.plot(x, cdf, 's-', markersize=10, fillstyle='none',
+            color=plt.cm.tab10(.15))
+    ax.plot(x, np.ones(len(x)) * threshold, 'k--', linewidth=3)
+
+    if show_first_PC:
+        PC1 = pca.components_[0]
+        plt.plot(np.linspace(1, 5, len(PC1)), -PC1*0.3 + min(cdf) + 0.1, 'k', linewidth=2)
+        text = ax.text(1, min(cdf) + 0.13, '$PC_1$', ha="left", va="center", size=20)
+
+    plt.xticks(x, fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.ylim(min(cdf) - 0.05, 1.02)
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    plt.xlabel('Number of Parameters', fontsize=22)
+    plt.ylabel(f'Cumultative\nExplained Variance', fontsize=22)
+    ax.tick_params(direction='in', width=2, length=8)
+    plt.show()
+
 def visualize_energy_points(plot, Energy, Refs, energy_points,
                             fontsize=20, ticks=(5, 10), label=None):
     """Visualize results from RFE on reference spectra."""
@@ -95,24 +134,28 @@ def visualize_energy_points(plot, Energy, Refs, energy_points,
         ax.set_title(label, fontsize=fontsize)
 
 def scale_coeffs_to_add_to_one(coeff_mtx):
+    """For every coeff list in coeff_mtx, scale to add to one."""
     return np.array([coeffs/np.sum(coeffs) for coeffs in coeff_mtx])
 
-def loss(coeffs, Refs, target, metric='mean_absolute_error'):
+def mae_loss(coeffs, Refs, target, metric='mean_absolute_error'):
     calc = Refs.T @ coeffs
-    calc = calc - np.min(calc)
+    calc = calc - np.min(calc)  # set min to zero
     return eval(metric)(calc, target) + 10*(np.sum(coeffs) - 1)**2
 
 def get_coeffs_from_sepctra(spectra, Refs):
+    """Find the coeffs that minimize spectral recontruction error."""
     m = Refs.shape[0]
     coeffs_0 = np.ones(m)/m
     bounds = np.zeros((m, 2))
     bounds[:, 1] = 1
-    coeffs = np.array([minimize(loss, coeffs_0, args=(Refs, spectrum), bounds=bounds)['x']
+    coeffs = np.array([minimize(mae_loss, coeffs_0,
+                       args=(Refs, spectrum), bounds=bounds)['x']
                        for spectrum in spectra])
     return scale_coeffs_to_add_to_one(coeffs)
 
-def plot_reconstructions(data, coeffs, m, Energy, Refs):
-    """Recon plot of spectra."""
+def plot_reconstructions(data, coeffs, m, Energy, Refs, metric='mean_absolute_error',
+                         verbose=True):
+    """Recon plot of spectra in a row."""
     fig, axes = plt.subplots(figsize=(5*m, 5), ncols=m)
     plt.subplots_adjust(wspace=0)
     for i in range(m):
@@ -125,4 +168,77 @@ def plot_reconstructions(data, coeffs, m, Energy, Refs):
         format_axis(ax, ticks=(10, 20), fontsize=20)
         ax.legend(fontsize=20, loc=4)
     plt.show()
-    print(f'Add to one? {np.sum(coeffs, axis=1)}')
+    metric_name = metric.replace('_', ' ')
+    if verbose:
+        print(f'{metric_name}: {eval(metric)(pred, true)}')
+        return eval(metric)(pred, true)
+
+def plot_spectra(Energy, data, mod=0, alpha=0.01):
+    """Spaghetti plot."""
+    fig, ax = plt.subplots(figsize=(10, 6))
+    for i, spectrum in enumerate(data):
+        if mod != 0:
+            if i%mod == 0:
+                ax.plot(Energy, spectrum , '-', linewidth=2, c='gray', alpha=alpha)
+        else:
+            ax.plot(Energy, spectrum , '-', linewidth=2, c='gray', alpha=alpha)
+    format_axis(ax)
+    plt.show()
+
+def two_dimensional_visualization(data, Refs, data_columns, N_refs=None, ncol=2,
+                                  method='PCA'):
+    """Plot dataset and Refs in 2D using dimensionality reduction."""
+    fig, ax = plt.subplots(figsize=(4, 4))
+
+    if method == 'PCA':
+        reducer = PCA(random_state=42, n_components=2)
+        reducer = reducer.fit(data)
+    elif method == 'UMAP':
+        reducer = umap.UMAP(random_state=42, n_components=2)
+        reducer = reducer.fit(data)
+    elif method == 't-SNE':
+        tsne = TSNE(random_state=42, n_components=2)
+        total_data = np.array([x for y in [data, Refs] for x in y])
+        transformed_data = tsne.fit_transform(total_data)
+
+    if method in ['PCA', 'UMAP']:
+        transformed = reducer.transform(data)
+        transformed_Refs = reducer.transform(Refs)
+    else:
+        transformed = transformed_data[:-len(Refs)]
+        transformed_Refs = transformed_data[-len(Refs):]
+
+    for i, coords in enumerate(transformed):
+        ax.plot(coords[0], coords[1], 'o', markersize=10, c='gray', alpha=0.05)
+
+    n = Refs.shape[0]
+    counts = 0
+    ele_to_idx_map = {'Cd': 2, 'Te': 8, 'Se': 6, 'Cl': 4, 'exp': 1, 'As': 0}
+
+    for i, coords in enumerate(transformed_Refs):
+        if N_refs is None:
+            label = data_columns[i]
+        else:
+            label = data_columns[N_refs[i]]
+        for key in ele_to_idx_map.keys():
+            if key in label:
+                j = ele_to_idx_map[key]
+                break
+        if n < 20:
+            c = counts
+        else:
+            c = j
+        color = plt.cm.tab20(c)
+        counts += 1
+        ax.plot(coords[0], coords[1], 'o', markersize=10, c=color, label=label)
+
+    ax.plot(np.average(transformed_Refs[:,0]),
+            np.average(transformed_Refs[:,1]),
+            'o', markersize=10, c='k', label='centroid')
+
+    ax.legend(fontsize=16, loc='center left', bbox_to_anchor=(1., .5), ncol=ncol)
+    ax.set_yticks([])
+    ax.set_xticks([])
+    ax.set_xlabel(f'${method}_1$', fontsize=18)
+    ax.set_ylabel(f'${method}_2$', fontsize=18)
+    plt.show()
